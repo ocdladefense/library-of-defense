4ca9802cd46f70a328cdcd2710322eba9d0db49d
Ubuntu <ubuntu@ip-172-31-22-227.us-west-2.compute.internal>
2021-12-16 00:33:23 +0000

    Show the errors for now.

--- LocalSettings.php
+++ LocalSettings.php
@@ -732,5 +732,6 @@ require_once('LocalSettingsOverrides.php');
 
 
 
-error_reporting(E_ALL);    
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
