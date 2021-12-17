b5484d917e3b836b583fd2bb08d3c496622af924
Ubuntu <ubuntu@ip-172-31-22-227.us-west-2.compute.internal>
2021-12-17 18:24:11 +0000

    Curious how these got into the repo, but removing them now.

--- extensions/clickpdx/autoloader/classes/PhpUtilities/DateFormats/DefaultDateFormat.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace PhpUtilities\DateFormats;
-
-class DefaultDateFormat extends DateFormat {
-	protected $format = 'Y-m-d';
-}
--- extensions/clickpdx/autoloader/classes/PhpUtilities/DateFormats/FriendlyDateFormat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PhpUtilities\DateFormats;
-
-class JQueryUIDefaultDateFormat extends DateFormat {
-
-	protected $format = "m/d/Y";
-
-}
